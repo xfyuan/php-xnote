fde8bb8a628509f81237130460fbcdc93777ead4
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2016-04-23 01:48:09 +0800

    add comments

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
+/**
+  * Loading all packagies
+  *
+  **/
 require_once __DIR__ . '/vendor/autoload.php';
 
-$source = './data/source.txt';
 
+// ----------------------------
+// read source data
+// ----------------------------
+$source = './data/source.txt';
 $data = file_get_contents($source);
 
+// ----------------------------
+// init conference
+// ----------------------------
 $conference = new App\Conference($data);
 
+// ----------------------------
+// generate scheduled tracks
+// ----------------------------
 $conference->scheduleTracksWithTalks();
-
 $conference->outputScheduledTracks();
